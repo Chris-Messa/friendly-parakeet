d0e674c8c02c76478b9429ca74bda4afc2ab6cf1
Chris Messa <blindsun95@gmail.com>
2023-03-18 15:32:36 -0500

    Set up initial functions

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,22 @@
 // Assignment code here
 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+function passPrompt(passwordCriteria) {
+  var critList = ["Lowercase letters", "Uppercase letters", "Numeric", "Special characters"];
+
+  for (var i = 0; i < critList.length; i++) {
+    window.prompt(`Do you want to include ${critList[i]}?`);
+  }
+
+
+  return passwordCriteria;
+}
+
+function generatePassword() {
+  
+}
 
 // Write password to the #password input
 function writePassword() {
