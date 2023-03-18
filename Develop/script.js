099366968af81954c12846337f825b2c97c43eea
Chris Messa <blindsun95@gmail.com>
2023-03-18 17:44:54 -0500

    implemented addLower function

--- Develop/script.js
+++ Develop/script.js
@@ -25,6 +25,22 @@ function passLengthPrompt(passLength) {
   return passLength;
 }
 
+function addLower() {
+  var critList = [];
+  if (lengthValidator()) {
+    var criteria = passPrompt();
+
+    if (criteria[0] === "YES") {
+      
+      critList.push("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+    } 
+    // if (criteria[1] === "YES") {
+
+    //   critList.push("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
+    // } 
+  }
+}
+
 function lengthValidator() {
   var validLength;
   var length = passLengthPrompt();
